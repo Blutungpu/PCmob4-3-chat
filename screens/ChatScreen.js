13504b9439be52b0b1dcc2c45bd1e7b5f68cccbb
blutungpu <blutungpu@gmail.com>
2022-06-16 13:04:56 +0800

    add

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import firebase from '../database/firebaseDB'
 
+const demoMessage =	{
+    _id: 1,
+    text: 'Hello there!',
+    createdAt: new Date(),
+    user: {
+        _id: 2,
+        name: 'Demo person',
+        avatar: 'https://placeimg.com/140/140/any'
+    }
+}
+const db = firebase.firestore().collection("messages");
 export default function ChatScreen() {
-  return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+  const[messages,setMessages] = useState([])
+  useEffect(() => {
+    const unsubscribe = db
+    .orderBy("createdAt", "desc")
+    .onSnapshot((collectionSnapshot) => {
+      const messages = collectionSnapshot.docs.map((doc) => {
+        const date = doc.data().createdAt.toDate();
+        const newDoc = { ...doc.data(), createdAt: date };
+        return newDoc;
+      });
+      setMessages(messages);
+    });
+  return unsubscribe;
+
+  })
+
+  function sendMessages(newMessages){
+ db.add(newMessages[0])
+  }
+    return (
+      <GiftedChat
+			messages={messages}
+			onSend={sendMessages}
+			renderUsernameOnMessage={true}
+			listViewProps={{
+				style: {
+				  backgroundColor: "#666",
+				},
+			}}
+			user={{
+				_id: 1,
+			}}
+		/>
+
+
   )
 }
 
